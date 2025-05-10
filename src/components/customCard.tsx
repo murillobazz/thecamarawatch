1ec745b9f89aadce12b4cf81c11ddee1fc3cb85b
murillobazz <murillobazilio@gmail.com>
2025-05-10 17:03:05 -0300

    NEW: Adiciona componente customCard para números da home

---
+++ src/components/customCard.tsx
@@ -0,0 +1,23 @@
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "./ui/skeleton";
+import { ReactElement } from "react";
+
+interface customCardProps {
+  children: ReactElement,
+  isLoading: boolean,
+}
+
+export default function CustomCard({ children, isLoading }: customCardProps) {
+  if (isLoading) {
+    return (
+      <Skeleton className="w-[360px] h-[120px]">
+      </Skeleton>
+    )
+  }
+
+  return (
+    <Card className="max-w-[360px]">
+      {children}
+    </Card>
+  )
+}
